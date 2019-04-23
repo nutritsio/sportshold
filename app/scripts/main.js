c0afb5c85bc26a56d97bb3954cf3045be6ff17e9
nutritsio <serhii.web.dev@gmail.com>
2019-04-23 13:30:21 +0300

    Feedback Block - lg

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
     $('.tabs li').click(function() {
         var tab_id = $(this).attr('data-tab');
-        
+
         $('.tabs li').removeClass('current');
-        $('.tab-content').removeClass('current');
+        $('.products .tab-content').removeClass('current');
 
         $(this).addClass('current');
-        $("#"+tab_id).addClass('current');
-    })
-})
+        $('#'+tab_id).addClass('current');
+    });
+
+    $('.tabs-testim li').click(function() {
+        var tab_id = $(this).attr('data-tab');
+        
+        switch(tab_id) {
+            case 'tab-testim-1':
+                $('.trugl')[0].style.margin = '0 28%';
+                break;
+            case 'tab-testim-2':
+                $('.trugl')[0].style.margin = '0 auto';
+                break;
+            case 'tab-testim-3':
+                $('.trugl')[0].style.margin = '0 68%';
+                break;
+        }
+
+        $('.tabs-testim li').removeClass('current');
+        $('.testimonials .tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $('#'+tab_id).addClass('current');
+    });
+});
