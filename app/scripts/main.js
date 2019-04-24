ce3fed264294ece566333f830e7cb7a04447fc00
nutritsio <serhii.web.dev@gmail.com>
2019-04-24 20:31:55 +0300

    News Block - lg

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,41 @@
 $(document).ready(function() {
-    $('.tabs li').click(function() {
+    var newsImgs = [
+        "url('../images/news-1.png')",
+        "url('../images/news-2.png')",
+        "url('../images/news-3.png')"
+    ];
+    
+    for (let i = 0; i < newsImgs.length; i++) {
+        $('.news .container .n_imgs .news-img')[i].style.backgroundImage = newsImgs[i]; 
+    }
+
+    $('.products .container .tabs li').click(function() {
         var tab_id = $(this).attr('data-tab');
 
-        $('.tabs li').removeClass('current');
-        $('.products .tab-content').removeClass('current');
+        $('.products .container .tabs li').removeClass('current');
+        $('.products .container .tab-content').removeClass('current');
 
         $(this).addClass('current');
         $('#'+tab_id).addClass('current');
     });
 
-    $('.tabs-testim li').click(function() {
+    $('.feedback .container .tabs-testim li').click(function() {
         var tab_id = $(this).attr('data-tab');
         
         switch(tab_id) {
             case 'tab-testim-1':
-                $('.trugl')[0].style.margin = '0 28%';
+                $('.trugl')[0].style.margin = '0 31%';
                 break;
             case 'tab-testim-2':
                 $('.trugl')[0].style.margin = '0 auto';
                 break;
             case 'tab-testim-3':
-                $('.trugl')[0].style.margin = '0 68%';
+                $('.trugl')[0].style.margin = '0 65%';
                 break;
         }
 
-        $('.tabs-testim li').removeClass('current');
-        $('.testimonials .tab-content').removeClass('current');
+        $('.feedback .container .tabs-testim li').removeClass('current');
+        $('.feedback .container .tab-content').removeClass('current');
 
         $(this).addClass('current');
         $('#'+tab_id).addClass('current');
